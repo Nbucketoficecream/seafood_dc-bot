80b664ac9cca891b315a88b70362af887b4ad18b
Nbucketoficecream <kevin20031111@gmail.com>
2023-08-16 15:06:10 +0800

    完成tag師傅

--- 功德機器人.py
+++ 功德機器人.py
@@ -1,16 +1,18 @@
+# coding: utf-8
+
 from discord.ext import commands
 import discord
 import json
 import asyncio
-
 intents = discord.Intents.default()
 intents.message_content = True
+intents.voice_states = True
 bot = commands.Bot(command_prefix='!',
                    intents=discord.Intents.all())
 
 with open('json1.json', "r", encoding="utf8") as file:
     data = json.load(file)
-
+seafood=data["monk"]
 @bot.event
 async def on_ready():
     print("Bot is ready")
@@ -19,25 +21,23 @@ async def on_ready():
 async def pray(ctx):
     await ctx.send(data['pray'])
 @bot.command()
+async def monk(ctx):
+      await ctx.send(f"<@{447739348473872384}> %s"%seafood)
+@bot.command()
 async def image(ctx):
     await ctx.send(file=discord.File(data['image']))
-
-@bot.command(
-    name='vuvuzela',
-    description='Plays an awful vuvuzela in the voice channel',
-    pass_context=True,
-)
-async def vuvuzela(context):
-    user = context.message.author
-    voice_channel = user.voice.channel
-
-    if voice_channel is not None:
-        vc = await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=data['vuvuzela.mp3']))
-        while vc.is_playing():
+@bot.command()
+async def play(ctx):
+    if ctx.author.voice and ctx.author.voice.channel:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()    
+        voice_client.play(discord.FFmpegPCMAudio(data["wood.mp3"]))
+        while voice_client.is_playing():
             await asyncio.sleep(1)
-        await vc.disconnect()
+        await voice_client.disconnect()
+        await ctx.send(data["gon"])
     else:
-        await context.send('User is not in a channel.')
+        await ctx.send(data["gon2"])
+
 
 bot.run(data['token'])
